3fd8e348fe6747a9507ad245b472b6cbbda506d1
Thembinkosi <thembinkosi.klein@gmail.com>
2019-05-02 16:31:08 +0200

    updates

--- www/UsbPRINT.js
+++ www/UsbPRINT.js
@@ -4,7 +4,6 @@ var exec = require('cordova/exec');
  * Sends the content to the printer.
  *
  * @param [ String ]   content  The plain/html text or a file URI.
- * @param [ Object ]   options  Options for the print job.
  * @param [ Function ] success  The success callback function.
  * @param [ Function ] error    The success callback function.
  */
